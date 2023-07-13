3550139cc2a212ad1f241277f902d84b433141c3
Pablo Galindo <pablogsal@gmail.com>
2023-07-13 13:16:56 +0100

    Remove thee MacOS trove classifier
    We do not support MacOS and this makes our users think we do
    
    Signed-off-by: Pablo Galindo <pablogsal@gmail.com>

--- setup.py
+++ setup.py
@@ -95,7 +95,6 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: POSIX :: Linux",
-        "Operating System :: MacOS",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
