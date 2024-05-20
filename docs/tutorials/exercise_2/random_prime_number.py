cc089b8f97ab236e32596add6c5351b370ad522b
Gwen Sarapata <gwendolynsarapata@gmail.com>
2024-05-20 17:55:37 -0400

    Add tutorial exercise 2 to find a prime number using a random number generator
    Signed-off-by: Gwen Sarapata <gwendolynsarapata@gmail.com>

---
+++ docs/tutorials/exercise_2/random_prime_number.py
@@ -0,0 +1,52 @@
+from math import sqrt
+import random
+
+
+'''
+Check if a number is prime
+Returns true if prime or False if composite
+'''
+def is_prime(number, itr=None):
+    # Generate iterator if no value is passed
+    if itr is None:
+        itr = int(sqrt(number)) + 1
+
+    # Prime number must be integers
+    if type(number) != int:
+        return False
+
+    # Base case for prime numbers
+    if itr == 1:
+        return True
+
+    # If the iterator evenly goes into the number, it is not prime
+    if number % itr == 0:
+        return False
+
+    # Recursively check with smaller iterator
+    if not is_prime(number, itr - 1):
+        return False
+
+    return True
+
+
+'''
+Generates a random integer 
+Returns number and prime value
+'''
+def generate_num():
+    number = random.random()
+    return number, is_prime(number)
+
+
+def main():
+    # Generate a new random number until a prime number is generated
+    number, prime = generate_num()
+    while not prime:
+        number, prime = generate_num()
+
+    print(f'The prime number generated is : {number}')
+
+
+if __name__ == '__main__':
+    main()
