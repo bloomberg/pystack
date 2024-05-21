8ef4e73fe8e449a5e60f93ea1a4b460d715cdc92
Vitor Raulino Kretzer <vitor.kretzer@gmail.com>
2024-05-21 13:28:36 -0300

    Handle the `NativeReportingMode.LAST` differently

--- src/pystack/traceback_formatter.py
+++ src/pystack/traceback_formatter.py
@@ -84,16 +84,22 @@ def format_thread(thread: PyThread, native_mode: NativeReportingMode) -> Iterabl
                 yield from format_frame(current_frame)
             current_frame = current_frame.next
     else:
-        yield from _format_merged_stacks(thread, current_frame)
+        yield from _format_merged_stacks(
+            thread, current_frame, native_mode == NativeReportingMode.LAST
+        )
     yield ""
 
 
 def _format_merged_stacks(
-    thread: PyThread, current_frame: Optional[PyFrame]
+    thread: PyThread,
+    current_frame: Optional[PyFrame],
+    native_last: bool = False,
 ) -> Iterable[str]:
+    c_frames_list: list[str] = []
     for frame in thread.native_frames:
         if frame_type(frame, thread.python_version) == NativeFrame.FrameType.EVAL:
             assert current_frame is not None
+            c_frames_list = []
             yield from format_frame(current_frame)
             current_frame = current_frame.next
             while current_frame and not current_frame.is_entry:
@@ -104,12 +110,18 @@ def _format_merged_stacks(
             continue
         elif frame_type(frame, thread.python_version) == NativeFrame.FrameType.OTHER:
             function = colored(frame.symbol, "yellow")
-            yield (
+            formatted_c_frame = (
                 f'    {colored("(C)", "blue")} File "{frame.path}",'
                 f" line {frame.linenumber},"
                 f" in {function} ({colored(frame.library, attrs=['faint'])})"
             )
+            if native_last:
+                c_frames_list.append(formatted_c_frame)
+            else:
+                yield formatted_c_frame
         else:  # pragma: no cover
             raise ValueError(
                 f"Invalid frame type: {frame_type(frame, thread.python_version)}"
             )
+    for c_frame in c_frames_list:
+        yield c_frame
