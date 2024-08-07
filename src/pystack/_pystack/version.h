5b7c726d473f7461a9a5882edcbd5b8434bdd205
Matt Wozniski <mwozniski@bloomberg.net>
2024-08-06 20:23:46 -0400

    Expose debug offsets in `python_v`
    This lays the groundwork for us to be able to adjust to changes in the
    layout of the debug offsets from one version of Python to the next.
    
    Signed-off-by: Matt Wozniski <mwozniski@bloomberg.net>

--- src/pystack/_pystack/version.h
+++ src/pystack/_pystack/version.h
@@ -64,6 +64,59 @@ struct py_runtime_v
     FieldOffset<remote_addr_t> o_interp_head;
     FieldOffset<GCRuntimeState> o_gc;
     FieldOffset<uintptr_t> o_tstate_current;
+
+    FieldOffset<uint64_t> o_dbg_off_runtime_state_struct_size;
+    FieldOffset<uint64_t> o_dbg_off_runtime_state_finalizing;
+    FieldOffset<uint64_t> o_dbg_off_runtime_state_interpreters_head;
+
+    FieldOffset<uint64_t> o_dbg_off_interpreter_state_struct_size;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_state_id;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_state_next;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_state_threads_head;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_state_gc;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_state_imports_modules;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_state_sysdict;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_state_builtins;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_state_ceval_gil;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_state_gil_runtime_state_locked;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_state_gil_runtime_state_holder;
+
+    FieldOffset<uint64_t> o_dbg_off_thread_state_struct_size;
+    FieldOffset<uint64_t> o_dbg_off_thread_state_prev;
+    FieldOffset<uint64_t> o_dbg_off_thread_state_next;
+    FieldOffset<uint64_t> o_dbg_off_thread_state_interp;
+    FieldOffset<uint64_t> o_dbg_off_thread_state_current_frame;
+    FieldOffset<uint64_t> o_dbg_off_thread_state_thread_id;
+    FieldOffset<uint64_t> o_dbg_off_thread_state_native_thread_id;
+    FieldOffset<uint64_t> o_dbg_off_thread_state_datastack_chunk;
+    FieldOffset<uint64_t> o_dbg_off_thread_state_status;
+
+    FieldOffset<uint64_t> o_dbg_off_interpreter_frame_struct_size;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_frame_previous;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_frame_executable;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_frame_instr_ptr;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_frame_localsplus;
+    FieldOffset<uint64_t> o_dbg_off_interpreter_frame_owner;
+
+    FieldOffset<uint64_t> o_dbg_off_code_object_struct_size;
+    FieldOffset<uint64_t> o_dbg_off_code_object_filename;
+    FieldOffset<uint64_t> o_dbg_off_code_object_name;
+    FieldOffset<uint64_t> o_dbg_off_code_object_qualname;
+    FieldOffset<uint64_t> o_dbg_off_code_object_linetable;
+    FieldOffset<uint64_t> o_dbg_off_code_object_firstlineno;
+    FieldOffset<uint64_t> o_dbg_off_code_object_argcount;
+    FieldOffset<uint64_t> o_dbg_off_code_object_localsplusnames;
+    FieldOffset<uint64_t> o_dbg_off_code_object_localspluskinds;
+    FieldOffset<uint64_t> o_dbg_off_code_object_co_code_adaptive;
+
+    FieldOffset<uint64_t> o_dbg_off_pyobject_struct_size;
+    FieldOffset<uint64_t> o_dbg_off_pyobject_ob_type;
+
+    FieldOffset<uint64_t> o_dbg_off_type_object_struct_size;
+    FieldOffset<uint64_t> o_dbg_off_type_object_tp_name;
+
+    FieldOffset<uint64_t> o_dbg_off_gc_struct_size;
+    FieldOffset<uint64_t> o_dbg_off_gc_collecting;
 };
 
 struct py_type_v
